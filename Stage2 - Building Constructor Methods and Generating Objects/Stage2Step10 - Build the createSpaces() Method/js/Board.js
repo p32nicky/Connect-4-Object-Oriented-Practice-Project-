73770844777c556207fb63a95fe09f6b56880f18
Nick Davies <nickdavies100@gmail.com>
2020-06-10 13:18:08 -0700

    Final Complete Version
    Final Complete Version

--- Stage2 - Building Constructor Methods and Generating Objects/Stage2Step10 - Build the createSpaces() Method/js/Board.js
+++
@@ -1,7 +0,0 @@
-class Board {
-    constructor() {
-        this.rows = 6;
-        this.columns = 7;
-        this.spaces = [];
-    }
-}
--- Stage2 - Building Constructor Methods and Generating Objects/Stage2Step10 - Build the createSpaces() Method/js/Space.js
+++
@@ -1,8 +0,0 @@
-class Space {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.id = `space-${x}-${y}`;
-        this.token = null;
-    }
-}
